b27376184a5d3f9fdbfe3ee21f84a8536383dd7d
Elias Riesinger <elias.riesinger@students.fh-hagenberg.at>
2024-08-21 20:31:35 +0200

    Serial test and approximate entropy test: fixed up the access_bits() method - previous implementation was overly complicated and also wrong.

--- sts-lib/src/tests/serial_and_approximate_entropy/mod.rs
+++ sts-lib/src/tests/serial_and_approximate_entropy/mod.rs
@@ -4,8 +4,8 @@
 use crate::bitvec::BitVec;
 use crate::BYTE_SIZE;
 
-pub mod serial;
 pub mod approximate_entropy;
+pub mod serial;
 
 /// Since the constraints for both test args are large the same, this function takes care of the validation.
 fn validate_test_arg(block_length: u8) -> Option<u8> {
@@ -22,176 +22,59 @@ fn validate_test_arg(block_length: u8) -> Option<u8> {
 ///
 /// start_idx is measured in bits.
 ///
-/// Bounds: start_idx < [BitVec::len_bit]
+/// Bounds: start_idx < [BitVec::len_bit], block_length <= [usize::BITS]
 fn access_bits(data: &BitVec, start_idx: usize, block_length: u8) -> Option<usize> {
-    /// Handles extracting the needed bits from the first byte. Return the start number.
-    #[inline]
-    fn first_byte(bits_todo: &mut u8, byte: u8, start_bit_idx: usize) -> usize {
-        let mask = (1 << (BYTE_SIZE - start_bit_idx)) - 1;
-        let first_byte = (byte as usize) & mask;
+    let data_len = data.len_bit();
 
-        *bits_todo -= (BYTE_SIZE - start_bit_idx) as u8;
-        first_byte << *bits_todo
-    }
-
-    /// Handles extracting the needed bits from the last byte.
-    #[inline]
-    fn last_byte(number: &mut usize, byte: u8, end_bit_idx: usize) {
-        // exclusive end index
-        let last_byte = byte >> (BYTE_SIZE - end_bit_idx);
-        *number |= last_byte as usize
-    }
-
-    if start_idx >= data.len_bit() {
+    if start_idx >= data_len || block_length as u32 > usize::BITS {
         return None;
     }
 
-    // convert index
-    let start_byte_idx = start_idx / BYTE_SIZE;
-    let start_bit_idx = start_idx % BYTE_SIZE;
+    let mut number: usize = 0;
+    let mut bits_left = block_length;
+    let mut idx = start_idx;
 
-    let end_byte_idx = (start_idx + block_length as usize - 1) / BYTE_SIZE;
-    let end_byte_idx = if end_byte_idx == data.data.len() {
-        let end_bit_idx = (start_idx + block_length as usize - 1) % BYTE_SIZE;
-        if end_bit_idx < data.remainder.len() {
-            end_byte_idx
-        } else {
-            end_byte_idx + 1
-        }
-    } else {
-        end_byte_idx
-    };
+    while bits_left > 0 {
+        idx %= data_len;
 
-    if end_byte_idx == start_byte_idx {
-        if start_byte_idx < data.data.len() {
-            // starts and ends in the same byte
-            let end_bit_idx = (start_idx + block_length as usize) % BYTE_SIZE;
+        let byte_idx = idx / BYTE_SIZE;
 
-            let byte = data.data[start_byte_idx] as usize;
-            let mask = (1 << (BYTE_SIZE - start_bit_idx)) - 1;
-            let byte = byte & mask;
+        if byte_idx < data.data.len() {
+            let bit_idx = idx % BYTE_SIZE;
+            let byte = data.data[byte_idx];
 
-            let number = if end_bit_idx != 0 {
-                byte >> (BYTE_SIZE - end_bit_idx)
+            if bit_idx == 0 && bits_left >= BYTE_SIZE as u8 {
+                // still inside the byte array, byte aligned storing possible
+                bits_left -= BYTE_SIZE as u8;
+
+                number |= (byte as usize) << bits_left;
+
+                idx += BYTE_SIZE;
             } else {
-                byte
-            };
+                // still inside the byte array, but not byte aligned
+                bits_left -= 1;
 
-            Some(number)
+                let bit = ((byte >> (BYTE_SIZE - bit_idx - 1)) & 1) != 0;
+
+                if bit {
+                    number |= 1 << bits_left;
+                }
+
+                idx += 1;
+            }
         } else {
-            // start_byte_idx == data.data.len() - exclusively from the last byte
-            let end_bit_idx = (start_idx + block_length as usize) % BYTE_SIZE;
-            let bit_length = end_bit_idx - start_bit_idx;
+            // in data.remainder
+            bits_left -= 1;
 
-            let mut number = 0;
+            let rem_idx = idx - data.data.len() * BYTE_SIZE;
 
-            for (i, &bit) in data.remainder[start_bit_idx..end_bit_idx].iter().enumerate() {
-                if bit {
-                    number |= 1 << (bit_length - i - 1)
-                }
+            if data.remainder[rem_idx] {
+                number |= 1 << bits_left;
             }
 
-            Some(number)
+            idx += 1;
         }
-    } else if end_byte_idx > data.data.len() {
-        // overflow, last bits may not be a full byte, adjustment ist needed
-        let additional_bits = start_idx + block_length as usize - data.len_bit();
-        let end_byte_idx = additional_bits / BYTE_SIZE;
-        let end_bit_idx = additional_bits % BYTE_SIZE;
-
-        // the necessary left shift for the next bits
-        let mut bits_todo = block_length;
-
-        // different depending on if the first bit is in data.data or data.remainder
-        let mut number = if start_byte_idx < data.data.len() {
-            // special case: first byte
-            let mut number = first_byte(&mut bits_todo, data.data[start_byte_idx], start_bit_idx);
-
-            // first loop to end of data.data
-            for &byte in &data.data[(start_byte_idx + 1)..] {
-                bits_todo -= BYTE_SIZE as u8;
-                number |= (byte as usize) << bits_todo;
-            }
-
-            // last byte of 'data'
-            for &bit in &data.remainder {
-                bits_todo -= 1;
-                if bit {
-                    number |= 1 << bits_todo;
-                }
-            }
-
-            number
-        } else {
-            let mut number = 0;
-
-            for &bit in &data.remainder[start_bit_idx..] {
-                bits_todo -= 1;
-                if bit {
-                    number |= 1 << bits_todo;
-                }
-            }
-
-            number
-        };
-
-        // second loop, starting from the beginning of data.data
-        for &byte in &data.data[0..end_byte_idx] {
-            bits_todo -= BYTE_SIZE as u8;
-            number |= (byte as usize) << bits_todo;
-        }
-
-        // special case: last byte
-        if end_bit_idx != 0 {
-            last_byte(&mut number, data.data[end_byte_idx], end_bit_idx);
-        }
-
-        Some(number)
-    } else if end_byte_idx == data.data.len() {
-        // the necessary left shift for the next bits
-        let mut bits_todo = block_length;
-
-        // special case: first byte
-        let mut number = first_byte(&mut bits_todo, data.data[start_byte_idx], start_bit_idx);
-
-        // loop to end of data.data
-        for &byte in &data.data[(start_byte_idx + 1)..] {
-            bits_todo -= BYTE_SIZE as u8;
-            number |= (byte as usize) << bits_todo;
-        }
-
-        // last byte of 'data'
-        for &bit in &data.remainder {
-            bits_todo -= 1;
-            if bit {
-                number |= 1 << bits_todo;
-            }
-
-            if bits_todo == 0 {
-                break;
-            }
-        }
-
-        Some(number)
-    } else {
-        // no overflow, conventional calculation
-        let end_bit_idx = (start_idx + block_length as usize) % BYTE_SIZE;
-
-        let mut bits_todo = block_length;
-
-        // special case: first byte
-        let mut number = first_byte(&mut bits_todo, data.data[start_byte_idx], start_bit_idx);
-
-        for &byte in &data.data[(start_byte_idx + 1)..end_byte_idx] {
-            bits_todo -= BYTE_SIZE as u8;
-            number |= (byte as usize) << bits_todo;
-        }
-
-        // special case: last byte
-        if end_bit_idx != 0 {
-            last_byte(&mut number, data.data[end_byte_idx], end_bit_idx);
-        }
-
-        Some(number)
     }
+
+    Some(number)
 }
